cdaa66659d134b50da5f21c03eb42d2b03f2dee1
v15hv4 <vishva2912@gmail.com>
2020-10-18 18:14:11 +0530

    Make main content view scrollable

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Navbar, NavbarBrand } from "reactstrap";
 
 export default () => {
     return (
-        <Navbar light className="custom-bg-primary">
+        <Navbar light className="custom-bg-primary mb-2">
             <NavbarBrand href="/" className="font-weight-bold custom-fg-primary">
                 ECSE
             </NavbarBrand>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,17 +12,19 @@ export default () => {
         <Container fluid>
             <Row className="mx-3">
                 <Col>
-                    {content.length > 0 ? (
-                        <Row>
-                            {content.map((item) => (
-                                <Col md={4} className="d-flex" key={item.url}>
-                                    <ProductItem {...item} />
-                                </Col>
-                            ))}
-                        </Row>
-                    ) : (
-                        <Greeting />
-                    )}
+                    <Container fluid className="overflow-auto" style={{ maxHeight: "90vh" }}>
+                        {content.length > 0 ? (
+                            <Row>
+                                {content.map((item) => (
+                                    <Col md={4} className="d-flex" key={item.url}>
+                                        <ProductItem {...item} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        ) : (
+                            <Greeting />
+                        )}
+                    </Container>
                 </Col>
                 <Col className="border-md border-left" md={4}>
                     <Chat setContent={setContent} />
