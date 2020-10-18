a2e08e3fa8a48f7f3d87a67f17c4c7b31c90b06f
v15hv4 <vishva2912@gmail.com>
2020-10-18 16:50:19 +0530

    Make chat input field functional

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,37 +1,54 @@
-import React from "react";
-import { Container, Input, Button } from "reactstrap";
+import React, { useState } from "react";
+import { Container, Input, Button, Form } from "reactstrap";
 
 import InMessage from "./InMessage";
 import OutMessage from "./OutMessage";
 
 export default () => {
-    var messages = [
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([
         {
+            id: 0,
             type: "in",
             content: "im1",
         },
         {
+            id: 1,
             type: "out",
             content: "om1",
         },
         {
+            id: 2,
             type: "in",
             content: "im2",
         },
-    ];
+    ]);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log(input);
+        setMessages([...messages, { id: messages.length + 1, type: "out", content: input }]);
+        setInput("");
+    };
 
     return (
         <Container fluid className="d-flex flex-column chatbar justify-content-between py-3">
             <Container fluid>
                 {messages.map((message) =>
-                    message.type === "in" ? <InMessage {...message} /> : <OutMessage {...message} />
+                    message.type === "in" ? (
+                        <InMessage key={message.id} {...message} />
+                    ) : (
+                        <OutMessage key={message.id} {...message} />
+                    )
                 )}
             </Container>
-            <Container fluid className="d-flex flex-row px-0">
-                <Input type="text" />
-                <Button color="primary" className="ml-2">
-                    Send
-                </Button>
+            <Container fluid className="px-0">
+                <Form className="d-flex flex-row" onSubmit={sendMessage}>
+                    <Input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+                    <Button color="secondary" className="ml-2">
+                        Send
+                    </Button>
+                </Form>
             </Container>
         </Container>
     );
--- client/src/components/OutMessage.jsx
+++ client/src/components/OutMessage.jsx
@@ -4,8 +4,8 @@ import { Card, CardBody, Row } from "reactstrap";
 export default ({ content }) => {
     return (
         <Row className="d-flex justify-content-end">
-            <Card>
-                <CardBody className="py-2 px-3">{content}</CardBody>
+            <Card className="bg-primary">
+                <CardBody className="py-2 px-3 text-light">{content}</CardBody>
             </Card>
         </Row>
     );
